ed0a57491c3245fb78c944fb67ec8790120bdb99
Gabor Szabo <gabor@szabgab.com>
2020-10-31 12:32:07 +0200

    get --limit as command line argument; shallow-clone the repositories

--- collect.py
+++ collect.py
@@ -1,15 +1,45 @@
+import argparse
 import os
 import re
+import tempfile
+import subprocess
 
 class Collector():
-    def __init__(self):
+    def __init__(self, args):
         self.root = os.path.dirname(os.path.abspath(__file__))
+        self.args = args
+        self.count = 0
 
     def run(self):
-        #print(self.root)
         repos = self.get_repos_iterator()
-        for repo in repos:
-            print(repo)
+        temp_dir = tempfile.mkdtemp()
+        print(temp_dir)
+        original_dir = os.getcwd()
+        os.chdir(temp_dir)
+        try:
+            for repo in repos:
+                print(repo)
+                if not self.clone_repository(repo):
+                    continue
+                self.count += 1
+                if self.args.limit and self.count >= self.args.limit:
+                    break
+
+
+        except Exception as err:
+            print(err)
+        os.chdir(original_dir)
+
+    def clone_repository(self, repo):
+        cmd = ["git", "clone", "--depth", "1", repo]
+        proc = subprocess.Popen(cmd,
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE,
+        )
+        _, _ = proc.communicate()
+        return proc.returncode == 0
+
+
 
     def get_repos_iterator(self):
         repos_file = os.path.join(self.root, 'repos.txt')
@@ -22,4 +52,7 @@ class Collector():
 
 
 if __name__ == '__main__':
-    Collector().run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--limit", type=int)
+    args = parser.parse_args()
+    Collector(args=args).run()
